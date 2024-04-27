eb38be4603080417dc231eb74bc214fe8b67b53b
Amina <90544857+Amina-mohd@users.noreply.github.com>
2024-04-27 20:26:00 +0400

    Update connect.php

--- ecoLogin/connect.php
+++ ecoLogin/connect.php
@@ -1,11 +1,10 @@
 <?php
-$serverName = "127.0.0.1"; // Replace it with localhost
-$port = 3307; // Replace with your MySQL server's port if it's not default or just remove it
+$serverName = "localhost";
 $username = "root";
 $password = "";
 $dbname = "ecorise"; //replace it with your database name
 
-$conn = mysqli_connect($serverName, $username, $password, $dbname, $port);
+$conn = mysqli_connect($serverName, $username, $password, $dbname);
 
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
